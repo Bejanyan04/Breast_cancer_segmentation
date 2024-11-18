520c05015347f2799f7ac143801748fa69e47d15
Anna Bejanyan <65034169+Bejanyan04@users.noreply.github.com>
2024-11-18 13:20:23 +0400

    Adding training stage

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 
 from src import logger 
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.pipeline.stage_02_model_training import ModelTrainingPipeline
+
 STAGE_NAME = 'data_ingestion'
 
 if __name__ == '__main__':
     try:
+        
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         obj = DataIngestionTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        
+        STAGE_NAME ='model training'
+        train_obj = ModelTrainingPipeline()
+        train_obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
     except Exception as e:
         logger.exception(e)
         raise e
